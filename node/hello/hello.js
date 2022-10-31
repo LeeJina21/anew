9f61b2999852e4110377e74e6bfbbea6fae507c4
LeeJina21 <80372072+LeeJina21@users.noreply.github.com>
2022-10-31 16:58:43 +0900

    U

--- node/hello/hello.js
+++ node/hello/hello.js
@@ -3,8 +3,8 @@ var app = express()
 
 app.get('/', function (req, res) {
 	res.send("Hello Node JS~!!")
-)}
+})
 
 app.listen(3000, function() {
 	console.log("3000 Port : Sever Started~!!");
-)}
+})
